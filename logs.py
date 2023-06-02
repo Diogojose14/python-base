ed7152d92fc96b21a361bf641d22bbdae88d89b0
DiogoJose <diogojose14@gmail.com>
2023-06-02 14:00:16 -0300

    salvando logs em arquivos

--- logs.py
+++ logs.py
@@ -2,20 +2,29 @@
 
 import os
 import logging
+from logging import handlers
 
 # BOILERPLATE
 # TODO: usar função
 # TODO: usar lib (loguru)
 log_level = os.getenv("LOG_LEVEL", "WARNING").upper()
 log = logging.Logger("diogo", log_level)
-ch = logging.StreamHandler() # COnsole/terminal/stderr
-ch.setLevel(log_level)
+#ch = logging.StreamHandler() # COnsole/terminal/stderr
+#ch.setLevel(log_level)
+fh = handlers.RotatingFileHandler(
+    "meulog.log",
+    maxBytes=300, # recomendação de tamanho 10**6
+    backupCount=10
+)
+fh.setLevel(log_level)
 fmt = logging.Formatter(
 	'%(asctime)s %(name)s %(levelname)s '
 	'l:%(lineno)d f:%(filename)s: %(message)s'
 )
-ch.setFormatter(fmt)
-log.addHandler(ch)
+#ch.setFormatter(fmt)
+fh.setFormatter(fmt)
+#log.addHandler(ch)
+log.addHandler(fh)
 
 
 """
@@ -26,7 +35,6 @@ log.error("Erro que afeta uma unica execucao")
 log.critial("Erro geral ex: banco de dados sumiu")
 """
 
-print("---")
 
 try:
 	1 / 0
